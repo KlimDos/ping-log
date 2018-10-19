8e8f6714496456edeeab04ce74ce5978d43210f8
Sasha Alimov <aalimov@axcient.com>
2018-10-20 01:29:44 +0300

    added logfile

--- ping-log.sh
+++ ping-log.sh
@@ -3,10 +3,10 @@ while true
 do
 if ping -c 1 $1 &> /dev/null
 then
-  echo "`date` $1 Ping is OK"
-  sleep 1
+  echo "`date` $1 Ping is OK" >> ping-log.log
+  sleep 5
 else  
-  echo "`date` $1 Ping is FAIL"
-  sleep 1
+  echo "`date` $1 Ping is FAIL" >> ping-log.log
+  sleep 5
 fi
 done
