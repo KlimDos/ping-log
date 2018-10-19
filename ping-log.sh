ddd6490845387b12c5c299a0c0879f82f5702ab2
Sasha Alimov <aalimov@axcient.com>
2018-10-20 01:51:47 +0300

    changed sleep time

--- ping-log.sh
+++ ping-log.sh
@@ -4,9 +4,9 @@ do
 if ping -c 1 $1 &> /dev/null
 then
   echo "`date` $1 Ping is OK" >> ping-log.log
-  sleep 5
+  sleep 2
 else  
   echo "`date` $1 Ping is FAIL" >> ping-log.log
-  sleep 5
+  sleep 2
 fi
 done
